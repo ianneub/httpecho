047b513ef250a0f711a68fcf6c20acfc8402935c
Ian Neubert <ian@ianneubert.com>
2015-10-19 11:10:52 -0700

    Allow setting long timeout in seconds with s query string.

--- long.go
+++ long.go
@@ -4,10 +4,17 @@ import (
   "fmt"
   "net/http"
   "time"
+  "strconv"
 )
 
 func longHandler(w http.ResponseWriter, r *http.Request) {
+  qs := r.URL.Query().Get("s")
+  seconds, err := strconv.Atoi(qs)
+  if (err != nil) {
+    seconds = 300
+  }
+
   fmt.Printf("Handling %s request for %s from %s..\n", r.Method, r.URL, r.RemoteAddr)
-  time.Sleep(5 * time.Minute)
+  time.Sleep(time.Duration(seconds) * time.Second)
   fmt.Fprintf(w, "Done.")
 }
